8581d34e0367e0c29cea81127640e54973f09752
Simon Carr <simon@soxprox.com>
2023-11-21 23:48:15 +0000

    Dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/feed', function () {
+    return view('feed');
+});
+
+Route::get('/profile', function () {
+    return view('users.profile');
+});
